bd4f20b3bc5e1f01b6c78ac6bb03f7804bd153de
Darshan Bhansali <msis.scu@gmail.com>
2020-04-30 14:30:38 -0700

    Updates

---
+++ precache-manifest.30e84ec79dfedf5f20a3507809ca012e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58361c7893aa9f42b5e50898eea834a",
+    "url": "/MSIS_SantaClaraUniversity_Website/index.html"
+  },
+  {
+    "revision": "561fd3cbdd791ca32aff",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ecf09734cb3ba46f7bf1",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/js/2.e2832a2b.chunk.js"
+  },
+  {
+    "revision": "34cfcdf0c0aaca82f9322ed2e486066b",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/js/2.e2832a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561fd3cbdd791ca32aff",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/js/main.bff754e4.chunk.js"
+  },
+  {
+    "revision": "67fe91be3769f6f4dc01",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/js/runtime-main.52c3a0fb.js"
+  },
+  {
+    "revision": "4512285b0dca3175b3d7ef77b10c3f03",
+    "url": "/MSIS_SantaClaraUniversity_Website/static/media/HorizontalRed1.4512285b.png"
+  }
+]);
